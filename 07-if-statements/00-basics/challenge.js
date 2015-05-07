aef9401fed61ce557853b4a8cbcc4366632f1003
amizony <amizony@gmail.com>
2015-05-07 08:36:02 +0000

    if-statements exercises

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,37 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav,guess) {
+   if (guess > fav) {
+      return "Too high";
+   } else if (guess < fav) {
+      return "Too low";
+   } else {
+      return "You got it!";
+   }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(a,b,c,d) {
+   var check = "incorrect";
+   if ((a===3) || (a === 5) || (a === 7)) {
+      if (b===2) {
+         if ((5 <= c) && (c <= 100)) {
+            if ((d < 9) || (20 < d)) {
+               check = "correct";
+            }
+         }
+      }
+   }
+   return check;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+   if ((item === "MacBook Air") && (money >= 999)) {
+      return true;
+   } else if ((item === "MacBook Pro") && (money >= 1299)) {
+      return true;
+   } else if ((item === "Mac Pro") && (money >= 2499)) {
+      return true;
+   } else if ((item === "Apple Sticker") && (money >= 1)) {
+      return true;
+   } else {
+      return false;
+   }
+};
