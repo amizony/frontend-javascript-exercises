5450eae1aba9c58adf0f5576097ae5093e068647
amizony <amizony@gmail.com>
2015-05-07 09:44:51 +0000

    arrays exercises

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,23 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+   var reverse = [1];
+   var j = 1;
+   for (var i = array.length - 1; i >= 0; i--) {
+      reverse[j] = array[i];
+      j += 1;
+   };
+   return reverse;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array) {
+   var str = "";
+   for (var i = 0; i < array.length-1; i++) {
+      str += array[i];
+      str += "+";
+   };
+   str += array[array.length-1];
+   return str;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array) {
+   return array.length+1;
+};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,29 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, list) {
+   var inList = false;
+   for (var i = 0; i < list.length; i++) {
+      if (item === list[i]) {
+         inList = true;
+      }
+   };
+   if (!inList) {
+      list.push(item);
+   }
+   return list;
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(list) {
+   var sorted = false;
+   var temp;
+   while (!sorted) {
+      sorted = true;
+      for (var i = 0; i < list.length-1; i++) {
+         if (list[i] < list[i+1]) {
+            temp = list[i];
+            list[i] = list [i+1];
+            list[i+1] = temp;
+            sorted = false;
+         }
+      };
+   }
+   return list;
+};
