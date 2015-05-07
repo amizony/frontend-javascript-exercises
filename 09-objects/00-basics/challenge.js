6687b2474a15e80b71cbd935267a0df32feffee1
amizony <amizony@gmail.com>
2015-05-07 11:40:34 +0000

    objects exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,38 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+   return {
+      title: courseTitle,
+      duration: courseDuration,
+      students: courseStudents,};
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+   if (!object.hasOwnProperty(newProp)) {
+      object[newProp] = newValue;
+   }
+   return object
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+   return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+   var item1 = {
+      name: 'MacBook Air',
+      price: 999,
+   };
+   var item2 = {
+      name: 'MacBook Pro',
+      price: 1299,
+   };
+   var item3 = {
+      name: 'Mac Pro',
+      price: 2499,
+   };
+   var item4 = {
+      name: 'Apple Sticker',
+      price: 1,
+   };
+   var canBuy = (((item1.name === item) && (money >= item1.price)) || ((item2.name === item) && (money >= item2.price)) || ((item3.name === item) && (money >= item3.price)) || ((item4.name === item) && (money >= item4.price)));
+   return canBuy;
+};
